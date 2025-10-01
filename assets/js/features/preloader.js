240b3fb2837d707f87b4db95235e3655931eaba8
Szunias <apexszunio@gmail.com>
2025-10-01 22:02:13 +0200

    ladniej

--- assets/js/features/preloader.js
+++ assets/js/features/preloader.js
@@ -5,24 +5,12 @@ window.App.preloader = (function($) {
     function init() {
         var $body = $('body');
 
-        // Remove preloader immediately - no waiting
-        // Preloader is mainly useful for first visit, after that it just slows down navigation
-        var removePreloader = function() {
+        // Remove preloader as soon as DOM is ready - don't wait for all resources
+        $(document).ready(function() {
             $body.removeClass('is-preload');
             $body.addClass('is-loaded');
             $body.removeClass('page-exit page-enter');
-        };
-
-        // Check if page was visited before (from cache/F5)
-        if (performance.navigation.type === 1 || sessionStorage.getItem('intro-seen')) {
-            // Page reload or already visited - remove preloader instantly
-            removePreloader();
-        } else {
-            // First visit - very short delay
-            $(document).ready(function() {
-                setTimeout(removePreloader, 50);
-            });
-        }
+        });
     }
 
     return {
